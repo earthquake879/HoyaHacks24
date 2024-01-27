a0226c4955e3c93f6e9bf210ca2e28555b19fac4
Kyra <pixelesc.com@gmail.com>
2024-01-27 10:27:50 -0500

    updated

--- index-script.js
+++ index-script.js
@@ -1,21 +1,32 @@
 const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-  
-  function success(pos) {
-    const crd = pos.coords;
-  
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-  }
-  
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
-  
-  navigator.geolocation.getCurrentPosition(success, error, options);
-  
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+// If user has not visited site before, ask for location permissions
+if (!localStorage.getItem('locationPerms') === null) {
+  console.log("First time?")
+
+  // Get permission
+  navigator.geolocation.getCurrentPosition((position) => {
+    // Set to true if it works
+    localStorage.setItem('locationPerms', true);
+  }, (error) => {
+    // It didn't work, set to false
+    localStorage.setItem('locationPerms', false);
+    console.log("Location permission denied, cannot continue.")
+  })
+}
